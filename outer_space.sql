88765132e59ba83f5fb8d23c340cd3ff486e8a73
jswwWorks <jsww.works@gmail.com>
2023-12-05 20:30:46 -0500

    Put insertions into outer space file

--- outer_space.sql
+++ outer_space.sql
@@ -17,6 +17,26 @@ planet_id VARCHAR(20) NOT NULL REFERENCES planets
 );
 
 
+INSERT INTO stars (star, temp_in_kelvin) VALUES
+    ('The Sun', 5800),
+    ('Proxima Centauri', 3042),
+    ('Gliese 876', 3192);
+
+
+INSERT INTO planets (planet, orbital_period_in_yrs, star_id) VALUES
+    ('Earth', 1.00, 'The Sun'),
+    ('Mars', 1.882, 'The Sun'),
+    ('Venus', 0.62, 'The Sun'),
+    ('Proxima Centauri b', 0.03, 'Proxima Centauri'),
+    ('Gliese 876 b', 0.236, 'Gliese 876');
+
+
+INSERT INTO moons (moon, planet_id) VALUES
+    ('The Moon', 'Earth'),
+    ('Phobos', 'Mars'),
+    ('Deimos', 'Mars');
+
+
 SELECT planet, star_id AS star, COUNT(moon) AS moon_count
     FROM planets
         LEFT OUTER JOIN moons
